62a5225f1ac1fe4a6455ce4ebcbd042c2c38ada2
yanntyb <69966391+yanntyb@users.noreply.github.com>
2020-12-01 14:28:32 +0100

    a

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 let i = 0;
 
+function verif(number){
+    let message = [" fizz"," buzz"," fizzbuzz"];
+    if ((number%3 === 0)&&(number%5!==0)){
+        return (number + message[0]);
+    }else if((i%5 === 0)&&(number%3!==0)){
+        return (number + message[1]);
+    }else if ((i%5 === 0) || (number%3 ===0)){
+        return (number + message[2]);
+    }else{
+        return (number);
+    }
+}
+
+function verif2(number){
+    let message = [" fizz"," buzz"," fizzbuzz"];
+    if(number%3 === 0){
+        if (number%5!==0){
+            return (number + message[0]);
+        }
+        else{
+            return (number + message[2]);
+        }
+    }else if (i%5 === 0){
+        return (number + message[1]);
+    }else {
+        return (number);
+    }
+}
+
+
 while(i<101){
-    let result = ((i%3 === 0)&&(i%5!==0)) ? console.log(i + " fizz") : ((i%5 === 0)&&(i%3!==0)) ? console.log(i + " buzz") : ((i%5 === 0) || (i%3 ===0)) ? console.log(i + " fizzbuzz") : ((i%5!==0)||(i%3!==0)) ? console.log(i) : console.log("error")
+    console.log(verif2(i));
     i++;
 }
